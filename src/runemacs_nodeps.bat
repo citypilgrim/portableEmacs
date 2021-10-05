03e600b6c50a2d136b225bffa07918dd1db1a22f
bboytian <94tian@gmail.com>
2021-10-05 14:50:09 +0800

    Changes > generalised the path setting for both nodeps and deps > Included .gitignore for reference of other people who are interested in using the repo
    Notes
    > Although the --debug-init flag is passed into src/runemacs*.bat, it seems like
    it doesnt work. Note sure if this is a pecularity of the windows start cmd

--- src/runemacs_nodeps.bat
+++ src/runemacs_nodeps.bat
@@ -1,19 +1,13 @@
-@echo off
-
+:: To be run after setting the HOME variable
 set emacs_version=%1
 set emacs_version=%emacs_version:"=%
 
 rem Setting env variables
 set HOME=%~dp0..\
-set git_path=%HOME%Git
-set git_bin=%git_path%\cmd
-set git_usr_bin=%git_path%\usr\bin
-
-set PATH=%git_bin%;%git_usr_bin%
-
+@call "%HOME%src\set_path"
 
 rem Temporarily installing dependencies
 @call "%HOME%src\temp_install_deps"
 
 rem Running emacs
-start "" "%HOME%%emacs_version%\Emacs\bin\runemacs.exe"
+start "" "%HOME%%emacs_version%\Emacs\bin\runemacs.exe" --debug-init
---
+++ src/set_path.bat
@@ -0,0 +1,10 @@
+:: To be run after setting the HOME variable
+set PATH=
+set PATH=%PATH%;C:\Program Files\Common Files\Oracle\Java\javapath
+set PATH=%PATH%;%SystemRoot%\system32
+set PATH=%PATH%;%SystemRoot%
+set PATH=%PATH%;%SystemRoot%\System32\Wbem
+set PATH=%PATH%;%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\
+set PATH=%PATH%;%SYSTEMROOT%\System32\OpenSSH\
+set PATH=%PATH%;%HOME%Git\cmd
+set PATH=%PATH%;%HOME%Git\usr\bin
